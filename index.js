cca613475010c2a452272f26db39f0c472fbac95
nicovon24 <nicovon24@gmail.com>
2025-07-04 12:50:29 -0300

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
-import { closePool } from "./data/db.js";
 import cors from "cors";
 import dotenv from "dotenv";
+import { closePool } from "./data/db.js";
 
 import eleccionesRouter from "./routes/elecciones.routes.js";
 import escuelaRouter from "./routes/escuela.routes.js";
@@ -9,43 +9,42 @@ import fiscalesRouter from "./routes/fiscales.routes.js";
 import incidenciasRouter from "./routes/incidencias.routes.js";
 import loginRouter from "./routes/login.routes.js";
 
-const app = express();
-
 dotenv.config();
-
+const app = express();
 const port = process.env.PORT || 3001;
 
+// Middlewares
 app.use(express.json());
-
 const allowedOrigins = [
 	"http://localhost:3000",
 	"http://192.168.1.105:3000",
 	"https://fiscalizacion2025.vercel.app",
 	"https://frontend-fiscales-lla-cba.vercel.app",
 ];
-
 app.use(cors({ origin: allowedOrigins, credentials: true }));
-app.options("*", cors({ origin: allowedOrigins, credentials: true }));
 
-app.listen(port, () => {
-	console.log(`Servidor levantado en puerto ${port}`);
-});
-
+// Rutas (solo paths relativos aquí)
 app.use("/elecciones", eleccionesRouter);
 app.use("/escuela", escuelaRouter);
 app.use("/fiscales", fiscalesRouter);
 app.use("/incidencias", incidenciasRouter);
 app.use("/login", loginRouter);
 
-// Cierre limpio en señales de sistema
+// Arranque
+const server = app.listen(port, () => {
+	console.log(`Servidor levantado en puerto ${port}`);
+});
+
+// Shutdown limpio
 const shutdown = async () => {
 	console.log("\nCerrando servidor y conexiones a la base de datos...");
-	await closePool(); // liberás conexiones
+	await closePool();
 	server.close(() => {
 		console.log("Servidor cerrado correctamente.");
 		process.exit(0);
 	});
 };
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
 
-process.on("SIGINT", shutdown); // Ctrl+C
-process.on("SIGTERM", shutdown); // kill, PM2, etc.
+export default app;
