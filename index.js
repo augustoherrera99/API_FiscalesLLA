ce0a79b4385fe1a7a0a43f7abcbefd01cfd9feb4
nicovon24 <nicovon24@gmail.com>
2025-07-04 11:48:03 -0300

    cors

--- index.js
+++ index.js
@@ -1,39 +1,51 @@
-import express from 'express'
-import { closePool } from './data/db.js';
+import express from "express";
+import { closePool } from "./data/db.js";
+import cors from "cors";
+import dotenv from "dotenv";
 
-import eleccionesRouter from './routes/elecciones.routes.js'
-import escuelaRouter from './routes/escuela.routes.js'
-import fiscalesRouter from './routes/fiscales.routes.js'
-import incidenciasRouter from './routes/incidencias.routes.js'
-import loginRouter from './routes/login.routes.js'
-const app = express()
-import dotenv from 'dotenv';
+import eleccionesRouter from "./routes/elecciones.routes.js";
+import escuelaRouter from "./routes/escuela.routes.js";
+import fiscalesRouter from "./routes/fiscales.routes.js";
+import incidenciasRouter from "./routes/incidencias.routes.js";
+import loginRouter from "./routes/login.routes.js";
 
-dotenv.config()
+const app = express();
+
+dotenv.config();
 
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
-app.listen(port, () =>{
-    console.log(`Servidor levantado en puerto ${port}`)
-})
+const allowedOrigins = [
+	"http://localhost:3000",
+	"http://192.168.1.105:3000",
+	"https://fiscalizacion2025.vercel.app",
+	"https://frontend-fiscales-lla-cba.vercel.app",
+];
 
-app.use('/elecciones', eleccionesRouter)
-app.use('/escuela', escuelaRouter)
-app.use('/fiscales', fiscalesRouter)
-app.use('/incidencias', incidenciasRouter)
-app.use('/login', loginRouter)
+app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.options("*", cors({ origin: allowedOrigins, credentials: true }));
+
+app.listen(port, () => {
+	console.log(`Servidor levantado en puerto ${port}`);
+});
+
+app.use("/elecciones", eleccionesRouter);
+app.use("/escuela", escuelaRouter);
+app.use("/fiscales", fiscalesRouter);
+app.use("/incidencias", incidenciasRouter);
+app.use("/login", loginRouter);
 
 // Cierre limpio en señales de sistema
 const shutdown = async () => {
-  console.log('\nCerrando servidor y conexiones a la base de datos...');
-  await closePool(); // liberás conexiones
-  server.close(() => {
-    console.log('Servidor cerrado correctamente.');
-    process.exit(0);
-  });
+	console.log("\nCerrando servidor y conexiones a la base de datos...");
+	await closePool(); // liberás conexiones
+	server.close(() => {
+		console.log("Servidor cerrado correctamente.");
+		process.exit(0);
+	});
 };
 
-process.on('SIGINT', shutdown);  // Ctrl+C
-process.on('SIGTERM', shutdown); // kill, PM2, etc.
+process.on("SIGINT", shutdown); // Ctrl+C
+process.on("SIGTERM", shutdown); // kill, PM2, etc.
